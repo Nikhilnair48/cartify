5d071ea8c69eb2d87adb1775ccc77926428a7365
Nikhilnair48 <nikhil.nair48@gmail.com>
2025-01-03 18:46:58 +0530

    fix: linting & formatting config

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+export default {
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": "./tsconfig.json",
+    "sourceType": "module"
+  },
+  "plugins": ["@typescript-eslint", "prettier"],
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  "rules": {
+    "@typescript-eslint/no-explicit-any": "off"
+  }
+}
